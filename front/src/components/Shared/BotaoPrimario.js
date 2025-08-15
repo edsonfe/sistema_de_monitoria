edf05a5a1ff966af758dfad55842b0d6bc344f55
Edson Ferreira <110269123+edsonfe@users.noreply.github.com>
2025-08-15 00:19:36 -0300

    Delete front directory

--- front/src/components/Shared/BotaoPrimario.js
+++
@@ -1,8 +0,0 @@
-// BotaoPrimario.jsx
-export default function BotaoPrimario({ text, onClick }) {
-  return (
-    <a href="#" className="btn" onClick={(e) => { e.preventDefault(); onClick(); }}>
-      {text}
-    </a>
-  );
-}
--- front/src/components/Shared/BotaoSecundario.js
+++
@@ -1,8 +0,0 @@
-// BotaoSecundario.jsx
-export default function BotaoSecundario({ text, to }) {
-  return (
-    <a href={to} className="btn2">
-      {text}
-    </a>
-  );
-}
